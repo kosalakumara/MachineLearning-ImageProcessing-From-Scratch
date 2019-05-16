da8868b5af3abb34161a5ce8dde9dfe67ea7c32c
jeevanthask <jeevanthanandika@gmail.com>
2019-05-16 16:50:58 +0530

    extended

---
+++ 04-linear-regression-showing-graph-predecting-multiple-values-in-a-csv-file/practise1 (2).py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+
+# In[11]:
+
+
+df = pd.read_csv(r"C:\Users\nandika\Desktop\homeprices.csv")
+
+
+# In[12]:
+
+
+df
+
+
+# In[27]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+plt.xlabel('area')
+plt.ylabel('price')
+
+
+# In[14]:
+
+
+plt.scatter(df.area,df.price,color = 'red',marker = '+')
+
+
+# In[15]:
+
+
+plt.show()
+
+
+# In[20]:
+
+
+reg = linear_model.LinearRegression()
+reg.fit(df[['area']],df.price)
+
+
+# In[30]:
+
+
+reg.coef_
+
+
+# In[32]:
+
+
+reg.intercept_
+
+
+# In[33]:
+
+
+3300 * 135.78 + 180616
+
+
+# In[34]:
+
+
+d = pd.read_csv(r"C:\Users\nandika\Desktop\area.csv")
+
+
+# In[35]:
+
+
+d
+
+
+# In[36]:
+
+
+d.head(3)
+
+
+# In[37]:
+
+
+reg.predict(d)
+
+
+# In[ ]:
+
+
+
+
